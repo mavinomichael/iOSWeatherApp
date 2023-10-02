da17d264efbfcd3d10ca66f62d0407777672419a
mavinomichael <mavinomicheal@gmail.com>
2023-10-02 16:18:03 +0100

    Initial Commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Mavino Michael on 02/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Mavino Michael on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
